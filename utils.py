81c9e9f5725142397f7fe20d0ed5d9404aec20dd
Guilherme Betta <gbetta.guilherme@gmail.com>
2026-04-10 12:05:40 -0300

    Visualizações, utils, analise descritiva
    Visualizções:
    1) As lógicas das células do notebook e visualizações foram convertidas em funções.
    2) Adicionaram-se 2 funções para facilitar o salvamento de figuras.
    3) As funções para geração de figuras foram movidas para um arquivo "visualizações.py"
    
    Utils:
    1) Atualizações para funcionamento adequado ao aplicar "from utils
    import..." em outros arquivos
    2) Adição da função primária para salvamento de figuras
    
    Análise descritiva:
    1) Seção de visualizações foi removida completamente
    2) Início da conversão de seus métodos em funções, para posterior implementação num arquivo tal como "utils" e "visualizacoes"

--- utils.py
+++ utils.py
@@ -7,21 +7,22 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 # Constantes
-METADADOS = ['Data', 'data_normalizada', 'Estatistica']
+METADADOS = ['Data', 'data_normalizada', 'Estatistica', 'Index']
 
 # Leitura de arquivos
 def dados(caminho_arquivo):
     """
     Carrega dados de arquivos Excel, e separa os metadados das variáveis.
+
     caminho_arquivo: Caminho relativo do arquivo no workspace.
     """
-
+    full_path = BASE_DIR / caminho_arquivo
     # Verifica a existência do arquivo desejado
     try:
-        df = pd.read_excel(caminho_arquivo)
+        df = pd.read_excel(full_path)
     # Retorna uma mensagem caso o arquivo não seja encontrado
     except FileNotFoundError:
-        raise FileNotFoundError(f"Arquivo não encontrado: {caminho_arquivo}")
+        raise FileNotFoundError(f"Arquivo não encontrado: {full_path}")
     
     # Separa os metadados presentes no dataframe das variaveis
     metadados = [col for col in METADADOS if col in df.columns]
@@ -29,11 +30,55 @@ def dados(caminho_arquivo):
     
     return df
 
+# Salvamento de arquivos
+def salvar(arquivo, nome_arquivo):
+    """
+    Salva o arquivo em data/out (no workspace), com timestamp.
+
+    arquivo: Seleciona o arquivo que deve ser salvo.
+    nome_arquivo: Nomeia o arquivo.
+    """
+
+    # Cria uma variável para guardar o caminho de onde o arquivo resultante será salvo
+    pasta_saida = BASE_DIR / "data" / "out"
+
+    # Cria um diretorio para salvar o arquivo caso ele ainda nao exista  
+    pasta_saida.mkdir(parents=True, exist_ok=True)  
+
+    # Gera o nome do arquivo de saida com timestamp de quando foi criado
+    nome_arquivo = str(nome_arquivo + f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx")
+
+    # Cria o diretório para salvar o arquivo caso ela ainda não exista
+    caminho_saida = pasta_saida / nome_arquivo
+
+    # Gera o arquivo de saida, sem a coluna de index, prevenindo colunas de index duplicadas
+    arquivo.to_excel(caminho_saida, engine='openpyxl', index=False)
+
+    # Gera uma mensagem para o usuario informando sobre onde o arquivo foi salvo
+    print(f"Arquivo salvo em: {caminho_saida}")
+
+# Salvemento de figuras
+def salvar_visualizacao(fig, nome_arquivo, formato="png", dpi=300):
+    pasta_saida = BASE_DIR / "data" / "out"
+    pasta_saida.mkdir(parents=True, exist_ok=True)
+
+    nome_arquivo = f"{nome_arquivo}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.{formato}"
+    caminho_saida = pasta_saida / nome_arquivo
+
+    fig.savefig(caminho_saida, dpi=dpi, bbox_inches="tight")
+    print(f"Visualização salva em: {caminho_saida}")
+    return caminho_saida
+
+
 # Pivot de dataframes
 def pivot(df, metadados, variaveis):
     """
     Pivot do dataframe, criando 3 colunas para cada variável, uma para cada estatística, 
-    assim eliminando a coluna "Estatistica" do dataframe
+    assim eliminando a coluna "Estatistica" do dataframe.
+
+    df: Seleciona o DataFrame a ser pivotado.
+    metadados: Pode manter como "metadados".
+    variaveis: Pode manter como "variaveis".
     """
     # Cria listas locais para separar metadados e variaveis do dataframe
     metadados = [col for col in METADADOS if col in df.columns]
@@ -68,27 +113,3 @@ def unpivot(df):
     # A fazer
     pass
 
-# Salvarmento de arquivos
-def salvar(arquivo, nome_arquivo):
-    """
-    Salva o arquivo em data/out (no workspace). O nome do arquivo é selecionado
-    com "nome_arquivo", e um timestamp é automaticamente é adicionado de acordo 
-    com o momento que o arquivo for salvo.
-    """
-    # Cria uma variável para guardar o caminho de onde o arquivo resultante será salvo
-    pasta_saida = Path("data/out")
-
-    # Cria um diretorio para salvar o arquivo caso ele ainda nao exista  
-    pasta_saida.mkdir(parents=True, exist_ok=True)  
-
-    # Gera o nome do arquivo de saida com timestamp de quando foi criado
-    nome_arquivo = str(nome_arquivo + f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx")
-
-    # Cria o diretório para salvar o arquivo caso ela ainda não exista
-    caminho_saida = pasta_saida / nome_arquivo
-
-    # Gera o arquivo de saida
-    arquivo.to_excel(caminho_saida, engine='openpyxl', index=True)
-
-    # Gera uma mensagem para o usuario informando sobre onde o arquivo foi salvo
-    print(f"Arquivo salvo em: {caminho_saida}")
---
+++ visualizacoes.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+from utils import salvar_visualizacao
+
+# Formatação dos boxplots
+cores_customizadas = {      
+    'boxes': 'Blue',
+    'medians': 'Red',
+    'whiskers': 'Yellow',
+    'caps': 'Green'
+}
+
+estilo_outliers = dict(             
+    markerfacecolor='magenta',    
+    marker='o',                   
+    alpha=0.5                     
+)
+
+# Função que auxilia no salvamento de figuras
+def salvar_plot(nome):
+    salvar_visualizacao(plt.gcf(), nome)
+    plt.show()
+    plt.close()
+
+def boxplot_tudo(df):
+    """
+    Gera boxplots para todas as variáveis numa única figura.
+    df: Seleciona o dataframe em que se deseja.
+    """
+    if 'Estatistica' in df.columns:
+        df.boxplot(by='Estatistica',             
+            layout=(5, 6),                  
+            figsize=(20,20),                
+            sharey=False,                 
+            rot=45,                       
+            color=cores_customizadas,     
+            flierprops=estilo_outliers,
+            notch=True,
+            )   
+        plt.suptitle('Distribuição das Variáveis por Estatística (Escalas Independentes)', fontsize=16, y=1.02)
+        plt.tight_layout()
+        salvar_plot('boxplot_tudo')
+    else:
+        print('Esse dataframe não apresenta coluna \'Estatistica\'. Talvez você tenha usado um dataframe pivotado?')
+
+def boxplot_estatisticas(df):
+    """
+    Cria boxplots separados para cada estatística (Min, Med, Max)
+    """
+    cols_min = [coluna for coluna in df.columns if coluna.endswith('Min.')] 
+    cols_med = [coluna for coluna in df.columns if coluna.endswith('Med.')] 
+    cols_max = [coluna for coluna in df.columns if coluna.endswith('Max.')]
+
+    # Min
+    df[cols_min].plot(kind='box', subplots=True, layout=(6,8), figsize=(20,20), sharey=False, color=cores_customizadas, flierprops=estilo_outliers)
+    plt.suptitle('Distribuição dos valores mínimos de cada variável (Escalas independentes)', fontsize=16, y=1.02)
+    plt.tight_layout()
+    salvar_plot("boxplots_min")
+
+    # Med
+    df[cols_med].plot(kind='box', subplots=True, layout=(6,8), figsize=(20,20), sharey=False, color=cores_customizadas, flierprops=estilo_outliers)
+    plt.suptitle('Distribuição dos valores médios de cada variável (Escalas independentes)', fontsize=16, y=1.02)
+    plt.tight_layout()
+    salvar_plot("boxplots_med")
+
+    # Max
+    df[cols_max].plot(kind='box', subplots=True, layout=(6,8), figsize=(20,20), sharey=False, color=cores_customizadas, flierprops=estilo_outliers)
+    plt.suptitle('Distribuição dos valores máximos de cada variável (Escalas independentes)', fontsize=16, y=1.02)
+    plt.tight_layout()
+    salvar_plot("boxplots_max")
+
+def visualizacoes_missingno(df):
+    """
+    Gera visualizações dos dados faltantes no DataFrame de interesse com os métodos da biblioteca missingno.
+    df: Seleciona o DataFrame de interesse.
+    """
+    msno.matrix(df) 
+    salvar_plot("matrix_msno")
+
+    msno.bar(df) 
+    salvar_plot('bar_msno')
+
+    msno.heatmap(df) 
+    salvar_plot('heatmap_msno')
+
+    msno.dendrogram(df)  
+    salvar_plot('dendogram_msno')
+
+def faltantes_ano(df):
+    """
+    Gera um heatmap de dados faltantes ao longo dos anos.
+    df: DataFrame de interesse.
+    """
+    colunas_data = [coluna for coluna in df.columns if str(df[coluna].dtype).startswith('datetime')]
+    missing_por_ano = (df.drop(columns=colunas_data).groupby(df["Data"].dt.year).apply(lambda x: x.isna().mean()*100))
+    missing_por_ano = missing_por_ano.T 
+
+    plt.figure(figsize=(18, 10))
+    sns.heatmap(missing_por_ano, cmap="RdBu_r", vmin=0, vmax=100, linewidths=0.5, linecolor="black")
+    plt.title("Taxa de dados faltantes (%) por variável e ano")
+    plt.xlabel("Ano")
+    plt.ylabel("Variável")
+    salvar_plot("faltantes_ano")
+
+def faltantes_mes(df):
+    """
+    Gera um heatmap de dados faltantes em função dos meses.
+    df: DataFrame de interesse.
+    """
+    colunas_data = [coluna for coluna in df.columns if str(df[coluna].dtype).startswith('datetime')]
+    missing_meses = (df.drop(columns=colunas_data)).groupby(df['Data'].dt.year).apply(lambda x: x.isna().sum())
+    missing_meses = missing_meses.T
+
+    plt.figure(figsize=(18, 10))
+    sns.heatmap(missing_meses, cmap="RdBu_r", vmin=0, vmax=12, linewidths=0.5, linecolor="black", cbar_kws={"label": "Meses faltantes"})
+    plt.title("Meses faltantes por variável e ano")
+    plt.xlabel("Ano")
+    plt.ylabel("Variável")
+    salvar_plot('faltantes_mes')

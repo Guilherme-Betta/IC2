4cb4a2bc28388749b1544a92598c9e4e280b6e1f
Guilherme Betta <gbetta.guilherme@gmail.com>
2026-04-23 11:45:14 -0300

    Gráficos de barras
    Pretende-se implementar gráficos de barras para visualizar as bases de dados em cada etapa do projeto, visando a facilitar a comparação entre elas.

---
+++ rascunho.py
@@ -0,0 +1,22 @@
+from utils import*
+import pandas as pd
+# import matplotlib as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dados_limpos = dados(r"data\out\3) Dados Limpos\dados_limpos20260420_161355.xlsx")
+
+iqr_knn = dados(r"data\out\5) Imputação\IQR\KNN\imput_knn20260420_163032.xlsx")
+
+iqr_media = dados(r"data\out\5) Imputação\IQR\Média\imput_mean20260420_163032.xlsx")
+
+iqr_mediana = dados(r"data\out\5) Imputação\IQR\Mediana\imput_median20260420_163032.xlsx")
+
+zscore_knn = dados(r"data\out\5) Imputação\Z-Score\KNN\imput_knn20260420_163302.xlsx")
+
+zscore_media = dados(r"data\out\5) Imputação\Z-Score\Média\imput_mean20260420_163302.xlsx")
+
+zscore_mediana = dados(r"data\out\5) Imputação\Z-Score\Mediana\imput_median20260420_163302.xlsx")
+
+dados_limpos.plot(
+    kind='bar')
